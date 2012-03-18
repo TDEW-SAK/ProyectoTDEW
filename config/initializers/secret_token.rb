10256ba9d9a94de8838596fa9c5a0711405d393b
Oscar Ramos <oscarramosp@gmail.com>
2012-03-18 10:51:12 -0500

    primer commit OR

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EligeTuProfe::Application.config.secret_token = 'd91e9ada2b35a766cf26fce9c18174f40bb92777f16cfa3fdcfc92cb249ce77926273b72521f3b411ea994fa06c7b604562924a6db1325f4bdd4dcf801199dff'
