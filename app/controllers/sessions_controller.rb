2a2ed7956b7a69972e2025e5765a1769a480f970
Julius <kujulius@hotmail.com>
2012-04-01 18:55:21 -0400

    Sesion FB Fix

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,16 +8,17 @@ class SessionsController < ApplicationController
     if session[:user_id]
       # Means our user is signed in. Add the authorization to the user
       User.find(session[:user_id]).add_provider(auth_hash)
-      
-      render :text => "You can now login using #{auth_hash["provider"].capitalize} too!"
+       session['name'] = auth.user.name
+#      render :text => "You can now login using #{auth_hash["provider"].capitalize} too!"
     else
       # Log him in or sign him up
       auth = Authorization.find_or_create(auth_hash)
       
       # Create the session
       session[:user_id] = auth.user.id
-      
-      render :text => "Welcome #{auth.user.name}!"
+      session['name'] = auth.user.name
+#      render :text => "Welcome #{auth.user.name}!"
+	  
     end
   end
 
@@ -27,6 +28,7 @@ class SessionsController < ApplicationController
   
   def destroy
     session[:user_id] = nil
+	session['name'] = nil
     render :text => "You've logged out!"
   end
 
